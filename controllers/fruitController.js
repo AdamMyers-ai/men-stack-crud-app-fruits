f9a024b73accb91e26963c4706558f1a46f09e8c
Adam Myers <adammyers002@gmail.com>
2026-01-09 11:56:48 -0600

    add css styling and group fruit routes in controllers folder

---
+++ controllers/fruitController.js
@@ -0,0 +1,64 @@
+const Fruit = require("../models/fruit.js");
+const express = require("express");
+const router = express.Router(); // allows us to attach all routes in one group
+
+// I.N.D.U.C.E.S
+
+// Index - GET /fruits - render all the fruits
+router.get("/fruits", async (req, res) => {
+  const allFruits = await Fruit.find();
+  res.render("fruits/index.ejs", { allFruits });
+});
+
+// New - GET /fruits/new - render the new fruits form
+router.get("/fruits/new", (req, res) => {
+  res.render("fruits/new.ejs");
+});
+
+// Delete - Delete /fruits/:fruitId - delete a specific fruit from the DB
+
+router.delete("/fruits/:fruitId", async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.fruitId);
+  res.redirect("/fruits");
+});
+
+// Update - PUT /fruits/:fruitId - update a specific fruit using req.body
+
+router.put("/fruits/:fruitId", async (req, res) => {
+  console.log(req.body);
+
+  req.body.isReadyToEat = req.body.isReadyToEat === "on" ? true : false;
+
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+
+  res.redirect(`/fruits/${req.params.fruitId}`);
+});
+
+// Create - POST /fruits - use the req.body to create a new fruit
+router.post("/fruits", async (req, res) => {
+  console.log(req.body);
+
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+
+  await Fruit.create(req.body);
+  res.redirect("/fruits/new");
+});
+
+// Edit - GET /fruits/:fruitId - render a populated form to edit the fruit
+
+router.get("/fruits/:fruitId/edit", async (req, res) => {
+  const fruit = await Fruit.findById(req.params.fruitId);
+  res.render("fruits/edit.ejs", { fruit });
+});
+
+// Show - GET /fruit/:fruitId - render a specific fruit from the DB
+router.get("/fruits/:fruitId", async (req, res) => {
+  const fruit = await Fruit.findOne({ _id: req.params.fruitId });
+  res.render("fruits/show.ejs", { fruit });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const app = express();
 const Fruit = require("./models/fruit");
 const morgan = require("morgan");
 const methodOverride = require("method-override");
+const path = require("path");
+const fruitController = require("./controllers/fruitController");
 
 // Middlewares
 require("./db/connection");
-
-app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // allow my app to use form data (adds form data to req.body)
 app.use(express.urlencoded({ extended: true }));
@@ -21,64 +22,7 @@ app.get("/", async (req, res) => {
   res.render("index.ejs");
 });
 
-// I.N.D.U.C.E.S
-
-// Index - GET /fruits - render all the fruits
-app.get("/fruits", async (req, res) => {
-  const allFruits = await Fruit.find();
-  res.render("fruits/index.ejs", { allFruits });
-});
-
-// New - GET /fruits/new - render the new fruits form
-app.get("/fruits/new", (req, res) => {
-  res.render("fruits/new.ejs");
-});
-
-// Delete - Delete /fruits/:fruitId - delete a specific fruit from the DB
-
-app.delete("/fruits/:fruitId", async (req, res) => {
-  await Fruit.findByIdAndDelete(req.params.fruitId);
-  res.redirect("/fruits");
-});
-
-// Update - PUT /fruits/:fruitId - update a specific fruit using req.body
-
-app.put("/fruits/:fruitId", async (req, res) => {
-  console.log(req.body);
-
-  req.body.isReadyToEat = req.body.isReadyToEat === "on" ? true : false;
-
-  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
-
-  res.redirect(`/fruits/${req.params.fruitId}`);
-});
-
-// Create - POST /fruits - use the req.body to create a new fruit
-app.post("/fruits", async (req, res) => {
-  console.log(req.body);
-
-  if (req.body.isReadyToEat === "on") {
-    req.body.isReadyToEat = true;
-  } else {
-    req.body.isReadyToEat = false;
-  }
-
-  await Fruit.create(req.body);
-  res.redirect("/fruits/new");
-});
-
-// Edit - GET /fruits/:fruitId - render a populated form to edit the fruit
-
-app.get("/fruits/:fruitId/edit", async (req, res) => {
-  const fruit = await Fruit.findById(req.params.fruitId);
-  res.render("fruits/edit.ejs", { fruit });
-});
-
-// Show - GET /fruit/:fruitId - render a specific fruit from the DB
-app.get("/fruits/:fruitId", async (req, res) => {
-  const fruit = await Fruit.findOne({ _id: req.params.fruitId });
-  res.render("fruits/show.ejs", { fruit });
-});
+app.use(fruitController);
 
 // route to catch any undefined urls (404)
 app.get("/*splat", (req, res) => {
